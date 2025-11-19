556f981d4c21e6bdaed3fb3f4489ed02968d6acb
novafaye <novafaye10@gmail.com>
2025-11-19 05:30:40 +0300

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const { REST, Routes } = require("discord.js");
+require("dotenv").config();
+
+const commands = [];
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("⏳ جاري نشر الأوامر...");
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands }
+    );
+    console.log("✅ تم نشر الأوامر بنجاح!");
+  } catch (error) {
+    console.error(error);
+  }
+})();
